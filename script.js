4dce22bad269368022bd0e559f7f5ebb620576a8
pin-0417 <s4023377@student.rmit.edu.au>
2025-09-14 02:10:01 +1000

    dice update

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 const dice = document.getElementById("dice");
 const rollBtn = document.getElementById("rollBtn");
+const audioPlayer = document.getElementById("audioPlayer");
 
 function rollDice() {
   let counter = 0;
   const rollInterval = setInterval(() => {
     const face = Math.floor(Math.random() * 6) + 1;
     dice.classList.add("rolling");
-    dice.src = `dice-${1}.png`;
-    dice.src = `dice-${2}.png`;
-    dice.src = `dice-${3}.png`;
-    dice.src = `dice-${4}.png`;
-    dice.src = `dice-${5}.png`;
-    dice.src = `dice-${6}.png`;
+    dice.src = `images/dice-${face}.png`;
     counter++;
     if (counter > 10) {
       clearInterval(rollInterval);
+      const finalFace = Math.floor(Math.random() * 6) + 1;
+      dice.src = `images/dice-${finalFace}.png`;
     }
   }, 100);
 }
